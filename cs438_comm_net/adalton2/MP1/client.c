85309c4df013bce49d00cd9ed88f7e3d601aaa97
Alex Dalton <alexmdalton@gmail.com>
2014-02-06 03:22:36 -0700

    mp1 done

--- cs438_comm_net/adalton2/MP1/client.c
+++ cs438_comm_net/adalton2/MP1/client.c
@@ -1,5 +1,6 @@
 /*
-** client.c -- a stream socket client demo
+** client.c -- a mastermind client
+** uses the stream socket client demo as base
 */
 
 #include <stdio.h>
@@ -14,9 +15,13 @@
 
 #include <arpa/inet.h>
 
-#define PORT "3490" // the port client will be connecting to 
+#define MAXDATASIZE 100 // max number of bytes a message can be
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+// Send to server function definition, sends data to server
+int send_to_server(int sockfd, void * buffer, int length);
+
+// Receive from server function definition, receives from server
+int receive_from_server(int sockfd, void * buffer, int bufsize);
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -30,82 +35,130 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
+    int sockfd;  
+    char buf[MAXDATASIZE];  // Receive buffer with size 100
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+    int i;
+    char guess[80];      // Input buffer from user
 
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
-	    exit(1);
-	}
+    // Check if enough arguments are given
+    if (argc != 3)
+    {
+        fprintf(stderr,"usage: client hostname port\n");
+        exit(1);
+    }
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
-
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
-	}
-
-	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-			s, sizeof s);
-	printf("client: connecting to %s\n", s);
-
-	freeaddrinfo(servinfo); // all done with this structure
-	    if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
-	        perror("recv");
-	        exit(1);
-	    }
-
-	    buf[numbytes] = '\0';
-
-	    printf("%s\n",buf);
-        int i;
-        for (i = 0; i < 8; i++)
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
         {
-	    /*if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1) {
-	        perror("recv");
-	        exit(1);
-	    }
-
-	    buf[numbytes] = '\0';
-
-	    printf("%s\n",buf);*/
-
-            int guess;
-            scanf("%i", &guess);
-            printf("%i\n", i);
-            if (send(sockfd, &guess, sizeof(int), 0) == -1) 
-                perror("send");
+            perror("client: socket");
+            continue;
         }
 
-        close(sockfd);
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
 
-	return 0;
+        break;
+    }
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+              s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    // Receive and display welcome message
+    buf[receive_from_server(sockfd, (void*) buf, sizeof(buf))] = '\0';
+    printf("%s\n",buf);
+
+    // Loop through process of 8 guesses
+    // For each guess 1 indication message is received
+    // 1 guess is sent, and 1 guess response is received
+    for (i = 0; i < 8; i++)
+    {
+        // Print indication from server to send a guess
+	buf[receive_from_server(sockfd, (void*) buf, sizeof(buf))] = '\0';
+	printf("%s",buf);
+
+        // Read up to 1kB from stdin, assume user doesn't enter in more
+        // If user does the overflow will count as a guess
+        fgets(guess, sizeof(guess), stdin);
+        guess[5] = '\0';
+        send_to_server(sockfd, (void*) guess, 5);
+
+        // Get the response from the server
+	buf[receive_from_server(sockfd, (void*) buf, sizeof(buf))] = '\0';
+	printf("%s",buf);
+
+        // Check for win
+        if (strcmp(buf, "You Win\n") == 0)
+        {
+            close(sockfd);
+            return 0;
+        }
+
+    }
+
+    close(sockfd);
+    return 0;
 }
 
+/*
+* Sends a message to the server
+* @param sockfd the socket file descriptor
+* @param buffer the message to send
+* @param length length of message
+* @return number of bytes sent, -1 if error
+*/
+int send_to_server(int sockfd, void * buffer, int length)
+{
+    if (send(sockfd, buffer, length, 0) == -1) 
+    {   
+        perror("send");
+        return -1; 
+    }   
+    return length;
+}
+
+/*
+* Receives a message from the server
+* @param sockfd the socket file descriptor
+* @param buffer holds received message
+* @param bufsize the size of passed in buffer
+* @return number of bytes received
+*/
+int receive_from_server(int sockfd, void * buffer, int bufsize)
+{
+    int numbytes;
+    if ((numbytes = read(sockfd, buffer, bufsize)) < 0)
+    {   
+        perror("recv");
+        exit(0);
+    }   
+    return numbytes;
+}
--- cs438_comm_net/adalton2/MP1/server.c
+++ cs438_comm_net/adalton2/MP1/server.c
@@ -1,5 +1,6 @@
 /*
-** server.c -- a stream socket server demo
+** server.c -- a mastermind server
+** uses the stream socket server demo as base
 */
 
 #include <stdio.h>
@@ -14,20 +15,18 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <pthread.h>
 #include <string.h>
 #include <time.h>
 
-#define PORT "3490"  // the port users will be connecting to
-
 #define BACKLOG 10	 // how many pending connections queue will hold
 
-int useRand = 1;
-int digits[] = {0, 0, 0, 0};
+// Play game function definition, main game function
+void play_game(int new_fd, int useRand, int * digits);
 
-void * listen_and_accept();
-void * play_game(int * new_fd);
+// Send to client function definition, sends data to client
 int send_to_client(int sockfd, void * buffer, int length);
+
+// Receive from client function definition, receives from client
 int receive_from_client(int sockfd, void * buffer, int bufsize);
 
 void sigchld_handler()
@@ -35,7 +34,7 @@ void sigchld_handler()
 	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-// get sockaddr, IPv4 or IPv6:
+// Get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -45,144 +44,244 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
+int main(int argc, char * argv[])
 {
-    pthread_t listener;
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+    int useRand = 0;
+    int digits[4]; // Variable to hold user defined code
+    char in_str[80]; // Reads input from the user 
 
-    pthread_create(&listener, NULL, listen_and_accept, NULL);
-    pthread_join(listener, NULL);
+    // Check if enough arguments are given
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: server port\n");
+        exit(1);
+    }
+
+    // Get random or digits from stdin
+    printf("Enter 'random' or 4 digits to be used in Mastermind\n");
+    fgets(in_str, sizeof(in_str), stdin);
+    // First four characters == "rand" use random
+    if (in_str[0] == 'r' && in_str[1] == 'a' && in_str[2] == 'n' &&
+        in_str[3] == 'd')
+    {
+      printf("Using random digits for all games\n");
+      useRand = 1;      
+    }
+    else
+    {
+      // Use a number based off first 4 characters of input
+      // If numbers everythign works fine otherwise, characters
+      // converted to a value between 0-5
+      digits[0] = (in_str[0] - '0') % 6;
+      digits[1] = (in_str[1] - '0') % 6;
+      digits[2] = (in_str[2] - '0') % 6;
+      digits[3] = (in_str[3] - '0') % 6;
+      printf("Using %i%i%i%i for all games\n", digits[0], digits[1],
+             digits[2], digits[3]);
+    }
+    
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
+        {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                       sizeof(int)) == -1)
+        {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "server: failed to bind\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (listen(sockfd, BACKLOG) == -1)
+    {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    // main accept() loop
+    while(1)
+    { 
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1)
+        {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+                  get_in_addr((struct sockaddr *)&their_addr),
+                  s, sizeof s);
+        printf("\nserver: got connection from %s\n", s);
+        if (!fork())
+        { // this is the child process
+            close(sockfd);
+            play_game(new_fd, useRand, digits);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
     return 0;
 }
 
-void * listen_and_accept()
+/*
+* Main game function for mastermind
+* Each client runs this function in their process
+* @param new_fd file descriptor for socket
+* @param useRand bool to use random code or not
+* @param digits array of user defined digits
+*/
+void play_game(int new_fd, int useRand, int * digits)
 {
-	int sockfd;  // listen on sock_fd, new connection on new_fd
-        int * new_fd;
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		//return 1;
-	}
-
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("server: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		//return 2;
-	}
-
-	freeaddrinfo(servinfo); // all done with this structure
-
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-
-	printf("server: waiting for connections...\n");
-
-	while(1) {  // main accept() loop
-		sin_size = sizeof their_addr;
-                new_fd = (int*)malloc(sizeof(int));
-		*new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (*new_fd == -1) {
-			perror("accept");
-			continue;
-		}
-
-		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
-		printf("\nserver: got connection from %s\n", s);
-                pthread_t game_session;
-
-		if (!fork()) { // this is the child process
-			close(sockfd); // child doesn't need the listener
-                        pthread_create(&game_session, NULL, play_game,
-                                       (void*) new_fd);
-                        pthread_join(game_session, NULL);
-			exit(0);
-		}
-		close(*new_fd);  // parent doesn't need this
-                free(new_fd);
-	}
-
-	//return 0;
-}
-
-void * play_game(int * new_fd)
-{
-    int game_digits[4];
-    int i;
-    int sockfd = *new_fd;
-    int guess;
+    int game_digits[4]; // code for this game
+    char guess[5];      // receive buffer for client guesses
+    int guess_digits[4]; // guessed digits from client
+    int white, black, i, j, k; // white and black count color/peg matches
+    int flag[4];        // marks whether a peg has already been counted
+    char out_str[30];   // output string to send to client
     srand(time(NULL));
+    // Generate code for game depending on whether rand or not
     printf("New game with digits: ");
     for (i = 0; i < 4; i++)
     {
         if (useRand == 1)
-        {
             game_digits[i] = rand() % 6;
-        }
         else
-        {
             game_digits[i] = digits[i];
-        }
         printf("%i", game_digits[i]);
     }
     printf("\n");
-    send_to_client(sockfd, (void*) "Welcome to Mastermind!\n", 23);
+
+    // Send welcome message
+    send_to_client(new_fd, (void*) "Welcome to Mastermind!\n", 23);
+    // Loop 8 times for 8 guesses of the game
     for (i = 0; i < 8; i++)
     {
-        //send_to_client(sockfd, (void*) "Make a guess: \n", 15);
-        receive_from_client(sockfd, (void*) &guess, sizeof(int));
-        printf("%i %i\n",i, guess);
+        white = 0; 
+        black = 0;
+        flag[0] = 0; flag[1] = 0; flag[2] = 0; flag[3] = 0;
+        sprintf(out_str, "Make guess %i/8: \n", i + 1);
+        // Send to client ready to receive guess
+        send_to_client(new_fd, (void*) out_str, sizeof(out_str));
+        // Receive from client their guess
+        receive_from_client(new_fd, (void*) guess, sizeof(guess));
+        // Convert guess to integers between 0 - 5
+        guess_digits[0] = (guess[0] - '0') % 6;
+        guess_digits[1] = (guess[1] - '0') % 6;
+        guess_digits[2] = (guess[2] - '0') % 6;
+        guess_digits[3] = (guess[3] - '0') % 6;
+        printf("Guess #%i: %i%i%i%i\n", i + 1, guess_digits[0], guess_digits[1],
+               guess_digits[2], guess_digits[3]);
+        // Count number of color matches and matching pegs
+        for (j = 0; j < 4; j++)
+        {
+            if (guess_digits[j] == game_digits[j])
+            {
+                flag[j] = 1;
+                black++;
+            }
+        }
+        for (j = 0; j < 4; j++)
+        {
+            if(guess_digits[j] != game_digits[j])
+            {
+                for(k = 0; k < 4; k++)
+                {
+                    if((k != j) && (guess_digits[j] == game_digits[k]) && !flag[k])
+                    {
+                        white++;
+                        flag[k] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        // Send win
+        if (black == 4)
+        {
+            send_to_client(new_fd, (void*) "You Win\n", 8);
+            return;
+        }
+        // Send lose
+        else if (i == 7)
+        {
+            send_to_client(new_fd, (void*) "You Lose\n", 9);
+            return;
+        }
+        // Send information
+        else
+        {
+            char send_str[40];
+            sprintf(send_str, "%i correct color+slot, %i correct colors\n",
+                    black, white);
+            send_to_client(new_fd, (void*) send_str, sizeof(send_str));
+        }
     }
 
-    close(sockfd);
-    //free(new_fd);
 }
 
+/*
+* Sends a message to the client
+* @param sockfd the socket file descriptor
+* @param buffer the message to send
+* @param length length of message
+* @return number of bytes sent, -1 if error
+*/
 int send_to_client(int sockfd, void * buffer, int length)
 {
     if (send(sockfd, buffer, length, 0) == -1)
@@ -193,13 +292,20 @@ int send_to_client(int sockfd, void * buffer, int length)
     return length;
 }
 
+/*
+* Receives a message from the client
+* @param sockfd the socket file descriptor
+* @param buffer holds received message
+* @param bufsize the size of passed in buffer
+* @return number of bytes received
+*/
 int receive_from_client(int sockfd, void * buffer, int bufsize)
 {
     int numbytes;
-    if ((numbytes = recv(sockfd, buffer, bufsize - 1, 0)) == -1)
+    if ((numbytes = read(sockfd, buffer, bufsize)) < 0)
     {
         perror("recv");
-        exit(1);
+        exit(0);
     }
     return numbytes;
 }
