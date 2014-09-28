1f7aecb0385b7014f7b5a52b5a6f63cae231a589
Alex Dalton <adalton2@illinois.edu>
2014-09-28 13:33:47 -0500

    exam gold code

---
+++ factorialGOLD.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+ int main(void)
+ {
+    int n; /* calculate from 1! to n! */
+              
+    /* Read n */
+    printf("Enter n: ");
+    scanf("%d", &n);
+                               
+    if ((n < 1) || (n > 30)) {
+        printf("Number out of range.\n");
+        return -1;
+    }
+                                                          
+    /* Calculate 1! to n! Insert your code here */
+    int i, j;
+    printf("1 = 1\n");
+    for (i = 2; i <= n; i++)
+    {
+        unsigned int factorial = 1;
+        printf("1 ");
+        for(j = 2; j <= i; j++)
+        {
+            printf("* %d ", j);
+            factorial *= j;
+        }
+        printf("= %d\n", factorial);
+    }
+    return 0;
+}
