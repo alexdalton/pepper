1fcebd7f39edb9248653bd782f9d249f2af6b512
Alex Dalton <alexmdalton@gmail.com>
2014-09-28 13:22:08 -0500

    gcf

---
+++ gcf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int value1, value2; // Calculate GCF of value1 and value2
+
+    // Read value1 and value2
+    printf("Enter two positive integers: ");
+    scanf("%d%d", &value1, &value2);
+
+    if (value1 < 1 || value2 < 1)
+    {
+        printf("Invalid value\n");
+        return -1;
+    }
+
+    /* Find and print the GCF of value1 and value2
+       Insert your code here */
+
+}
