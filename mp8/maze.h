e99492e84c8bf82b7b9e61402a93eb2825bbd8b6
Alex Dalton <alexmdalton@gmail.com>
2015-03-10 22:08:15 -0500

    added mp8

---
+++ mp8/maze.h
@@ -0,0 +1,4 @@
+void findStart(char ** maze, int width, int height, int * x, int * y);
+void printMaze(char ** maze, int width, int height);
+int solveMazeDFS(char ** maze, int width, int height, int xPos, int yPos);
+int checkMaze(char ** maze, int width, int height);
